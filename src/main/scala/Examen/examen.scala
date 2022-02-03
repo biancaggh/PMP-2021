9b9710874c8654593985b68e647bd9f23130e2fe
Gheorghițanu Bianca-Gabriela <79106145+biancaggh@users.noreply.github.com>
2022-02-03 10:49:45 +0200

    examen

--- src/main/scala/Examen/examen.scala
+++ src/main/scala/Examen/examen.scala
@@ -14,9 +14,9 @@ object T1 {
 		val calitate=Select (0.27 -> 'mica, 0.6 -> 'medie, 0.13-> 'mare)
 	}
 
-    class Nominalizare(val at: Autor, val ab: Album){
+    class Nominalizare( val ab: Album){
         def getProb(){
-            val nominalizat=Chain(at.pupular,ab.calitate,(at: Boolean,ab:Boolean) => {
+            val nominalizat=Chain(at,ab.calitate,(at: Boolean,ab:Boolean) => {
                 if(!at && ab=="mica") Flip(0.013)
                 else if(at && ab=="mica") Flip(0.014)
                 else if(!at && ab=="medie") Flip(0.016)
@@ -24,13 +24,20 @@ object T1 {
                 else if(!at && ab=="mare") Flip(0.047)
                 else Flip(0.18)
             }
+            return nominalizat
             )
         }
     }
 	
 	def main(args: Array[String]) {
+        val albumm= new Album()
 		val autori=List(1,2,3,4,5)
-        val albume=10
+        val albume=List.tabulate(10)((aut: Int) => ^^(aut,albumm))
+        val nom: Array[Element[Int]] = Array.fill(10)(Constant(1))
+        for {alm <- 1 until albume}{
+            nom(alm)=Nominalizare(alm)
+        }
+        println(VariableElimination.probability(nom(1), true))
 
 		
 	}
